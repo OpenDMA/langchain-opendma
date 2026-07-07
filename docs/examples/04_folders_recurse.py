276e110d17c61e08f940ff52c7c165e8a47aa970
Stefan Kopf <s.kopf@xaldon.de>
2026-07-07 16:00:15 +0200

    Added examples showing folder handling

---
+++ docs/examples/04_folders_recurse.py
@@ -0,0 +1,52 @@
+"""
+Basic usage example of OpenDMALoader connecting to the tutorial-xmlrepo.
+
+Run the tutorial REST service docker container:
+```
+docker run -p 8080:8080 ghcr.io/opendma/tutorial-xmlrepo:0.8.1
+```
+It will provide the tutorial xml repository. Make sure that this service is available by opening  
+http://localhost:8080/opendma  
+in a web browser.
+"""
+
+from langchain_opendma import OpenDMALoader
+
+print(f"recurse_folders=False")
+
+loader = OpenDMALoader(
+    endpoint="http://localhost:8080/opendma",
+	username="ignored",
+    password="ignored",
+    repository_id="sample-repo",
+    folder_ids=[ "sample-folder-root" ],
+    recurse_folders=False,
+    include_no_content=True,
+    include_unhandled_content=True
+)
+
+documents = loader.load()
+print(f"Loaded {len(documents)} documents")
+
+for doc in documents:
+    print(f"ID: {doc.metadata.get('opendma:Id')}")
+
+print(f"\nrecurse_folders=True")
+
+loader = OpenDMALoader(
+    endpoint="http://localhost:8080/opendma",
+	username="ignored",
+    password="ignored",
+    repository_id="sample-repo",
+    folder_ids=[ "sample-folder-root" ],
+    recurse_folders=True,
+    include_no_content=True,
+    include_unhandled_content=True
+)
+
+documents = loader.load()
+print(f"Loaded {len(documents)} documents")
+
+for doc in documents:
+    print(f"ID: {doc.metadata.get('opendma:Id')}")
+
