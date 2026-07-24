af1173cac73ff5fbfdf30e9d54e03e2cdc56191d
Stefan Kopf <s.kopf@xaldon.de>
2026-07-24 09:36:06 +0200

    Bump version to next dev iteration

--- src/langchain_opendma/__init__.py
+++ src/langchain_opendma/__init__.py
@@ -14,7 +14,7 @@ from langchain_opendma.content_handlers import (
 )
 from langchain_opendma.loaders import AlfrescoLoader, OpenDMALoader
 
-__version__ = "0.1.0"
+__version__ = "0.2.0.dev1"
 
 __all__ = [
     "OpenDMALoader",
